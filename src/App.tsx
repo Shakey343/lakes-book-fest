62ecaf6e1be1685e5335daf6cbc61f61f99ba48c
Jake Pople <jakepople@hotmail.co.uk>
2024-10-05 12:17:28 +0100

    updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -31,12 +31,12 @@ function App() {
         </div>
 
         <div className="flex flex-col px-[80px] pt-[80px] pb-[120px] bg-silver text-night">
-          <div className="w-fit mb-[60px]">
+          <div className="w-fit mb-[60px] leading-none">
             <h2 className="relative text-[54px] font-light">
               The Festival
-              <div className="absolute right-[76.7px] -top-[5.8px] w-[5.2px] h-[5.2px] rounded-full bg-fire-red"></div>
+              <div className="absolute right-[76.7px] top-[7px] w-[5.2px] h-[5.2px] rounded-full bg-fire-red"></div>
             </h2>
-            <h3 className="text-[30px] font-light leading-loose">
+            <h3 className="text-[30px] font-light">
               12th-14th June, 20
               <span className="text-jonquil font-medium">26</span>
             </h3>
@@ -64,33 +64,33 @@ function App() {
         <div className="px-[80px] py-[40px] h-[800px] bg-map-grey relative">
           <div className="absolute top-0 left-0 bg-map-grey w-[470px] h-full z-10"></div>
           <div className="z-20 relative w-fit">
-            <h2 className="text-[44px] text-white font-light leading-tight mb-[40px]">
+            <h2 className="text-[44px] text-white font-light mb-[40px]">
               Location &<br /> Links
             </h2>
             <div className="flex flex-col gap-[30px]">
               <div className="flex flex-col gap-[10px]">
                 <h3 className="text-[32px] font-light">Closest Train Stations</h3>
-                <div>
-                  <p>Cark in Cartmel - 5 minutes drive</p>
-                  <p>Oxenholme - 30 minutes</p>
-                  <p>Lancaster - 40 minutes</p>
-                </div>
+                <ul className="leading-relaxed">
+                  <li>Cark in Cartmel - 5 minutes drive</li>
+                  <li>Oxenholme - 30 minutes</li>
+                  <li>Lancaster - 40 minutes</li>
+                </ul>
               </div>
               <div className="flex flex-col gap-[10px]">
                 <h3 className="text-[32px] font-light">By Car</h3>
-                <div>
-                  <p>Cark in Cartmel - 5 minutes drive</p>
-                  <p>Oxenholme - 30 minutes</p>
-                  <p>Lancaster - 40 minutes</p>
-                </div>
+                <ul className="leading-relaxed">
+                  <li>Cark in Cartmel - 5 minutes drive</li>
+                  <li>Oxenholme - 30 minutes</li>
+                  <li>Lancaster - 40 minutes</li>
+                </ul>
               </div>
               <div className="flex flex-col gap-[10px]">
                 <h3 className="text-[32px] font-light">By Train</h3>
-                <div>
-                  <p>Cark in Cartmel - 5 minutes drive</p>
-                  <p>Oxenholme - 30 minutes</p>
-                  <p>Lancaster - 40 minutes</p>
-                </div>
+                <ul className="leading-relaxed">
+                  <li>Cark in Cartmel - 5 minutes drive</li>
+                  <li>Oxenholme - 30 minutes</li>
+                  <li>Lancaster - 40 minutes</li>
+                </ul>
               </div>
             </div>
           </div>
@@ -101,7 +101,16 @@ function App() {
           />
         </div>
 
-        <div className="px-[80px] py-[40px]"></div>
+        <div className="px-[80px] py-[40px] flex flex-col gap-[40px]">
+          <h2 className="text-[44px] font-light">About Cartmel</h2>
+          <h3 className="quote">A thimble full of diamonds</h3>
+          <ul className="py-[60px] list-disc list-inside marker:text-fire-red">
+            <li>Cartmel Priory (est. 1189)</li>
+            <li>A host of pubs, cafés and artisanal food shops</li>
+            <li>L'Enclume (Michelin 3-star restaurant, Head Chef Simon Rogan)</li>
+            <li>Cartmel Sticky Toffee Pudding</li>
+          </ul>
+        </div>
 
         <div className="px-[80px] py-[40px]"></div>
       </main>
--- src/components/WhoCards.tsx
+++ src/components/WhoCards.tsx
@@ -42,11 +42,11 @@ const WhoCards = () => {
   return team.map((member, i) => (
     <a
       key={i}
-      className="flex items-center flex-col gap-[30px] w-[360px] h-[432px] group"
+      className="flex items-center flex-col gap-[30px] w-[360px] min-h-[432px] group"
       href={member.webUrl}
       target="_blank"
     >
-      <h3 className="text-white text-3xl">{member.name}</h3>
+      <h3 className="text-white text-3xl text-center">{member.name}</h3>
       <img
         className="w-[300px] h-[300px] rounded-full object-cover border-2 border-jonquil group-hover:border-8 transition-all"
         src={member.imgUrl}
