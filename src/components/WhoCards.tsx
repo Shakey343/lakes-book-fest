4208acaa9e7fc38c54a9dda309988336a948172d
Jake Pople <jakepople@hotmail.co.uk>
2024-10-05 11:00:27 +0100

    hover on who cards

--- src/components/WhoCards.tsx
+++ src/components/WhoCards.tsx
@@ -8,6 +8,7 @@ type Team = {
   role: string;
   imgUrl: string;
   imgUrlAlt: string;
+  webUrl: string;
 }[];
 
 const team: Team = [
@@ -16,31 +17,34 @@ const team: Team = [
     about: "Award-winning author & journalist",
     role: "Festival Director",
     imgUrl: ChristopherImg,
-    imgUrlAlt: "Profile image Christopher de Bellaigue Smiling"
+    imgUrlAlt: "Profile image Christopher de Bellaigue Smiling",
+    webUrl: "https://www.christopherdebellaigue.com/"
   },
   {
     name: "The Racecourse, Cartmel",
     about: "Owned by Holker Group & the Cavendish family",
     role: "The Location",
     imgUrl: RacecourseImg,
-    imgUrlAlt: "Image of horse jumping"
+    imgUrlAlt: "Image of horse jumping",
+    webUrl: "https://cartmel-racecourse.co.uk/"
   },
   {
     name: "Charlotte Fairbairn",
     about: "Novelist & Creative Director, Lowther Castle",
     role: "Festival Producer",
     imgUrl: CharlotteImg,
-    imgUrlAlt: "Profile image of Charlotte Fairbairn with her dog - Piglet"
+    imgUrlAlt: "Profile image of Charlotte Fairbairn with her dog - Piglet",
+    webUrl: "https://www.charlottefairbairn.co.uk/"
   },
 ];
 
 const WhoCards = () => {
   return team.map((member, i) => (
-    <div key={i} className="flex items-center flex-col gap-[30px]">
+    <a key={i} className="flex items-center flex-col gap-[30px] w-[360px] h-[432px] group" href={member.webUrl} target="_blank">
       <h3 className="text-white text-3xl">{member.name}</h3>
-      <img className="w-[300px] h-[300px] rounded-full object-cover border-2 border-jonquil" src={member.imgUrl} alt={member.imgUrlAlt} />
-      <h4 className="text-night text-[22px] text-black-olive">{member.role}</h4>
-    </div>
+      <img className="w-[300px] h-[300px] rounded-full object-cover border-2 border-jonquil group-hover:border-8 transition-all" src={member.imgUrl} alt={member.imgUrlAlt} />
+      <h4 className="text-night text-[22px] text-black-olive group-hover:text-[24px]">{member.role}</h4>
+    </a>
   ));
 };
 
